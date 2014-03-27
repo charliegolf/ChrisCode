23c75628ba6cd9f8aa4b9890fee0346a1cb9d5dd
charliegolf <charliegolf@talktalk.net>
2014-03-27 15:55:35 +0000

    working on a new approach to automate the combo box

--- ExperimentInFormAutomation/ExperimentInFormAutomation/AutomateListBox.cs
+++ ExperimentInFormAutomation/ExperimentInFormAutomation/AutomateListBox.cs
@@ -4,22 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Automation;
+using System.Windows.Automation.Peers;
 
 namespace ExperimentInFormAutomation
 {
     class AutomateListBox
     {
-        public void AutomateThisListBox(AutomationElement aeForm, int item )
-        {
+       // public void AutomateThisListBox(AutomationElement aeForm, int item )
+        //{
+            /*
             AutomationElement aeListBox = aeForm.FindFirst(TreeScope.Children,
-            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.List));
+            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.ComboBox));
 
-            AutomationElementCollection aeListBoxItems = aeListBox.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.  .ControlTypeProperty, ControlType.ListItem));
+            AutomationElementCollection aeListBoxItems = aeListBox.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.ListItem));
 
-            AutomationElement aeFirstItem =  aeListBoxItems[item];
+            AutomationElement aeFirstItem =  aeListBoxItems[3];
             
             string s = (string)aeFirstItem.GetCurrentPropertyValue(AutomationElement.NameProperty);
-        }
- 
+
+          */
+
+        public void AutomateThisListBox(AutomationElement aeForm, int item)
+        {
+        if(aeForm == null)
+        throw new Exception("Combo Box not found");
+
+        //Get the all the list items in the ComboBox
+        AutomationElementCollection comboboxItem = aeForm.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.ListItem));
+
+        //Expand the combobox
+
+        ComboBoxAutomationPeer peer =
+         new ComboBoxAutomationPeer(aeComboBox);
+        IExpandCollapseProvider provider =
+          (IExpandCollapseProvider)
+          peer.GetPattern(PatternInterface.ExpandCollapse);
+        provider.Expand();
+       // ExpandCollapsePattern expandPattern = (ExpandCollapsePattern)aeForm.GetCurrentPattern(ExpandCollapsePattern.Pattern);
+       // expandPattern.Expand();
+
+        //Index to set in combo box
+        AutomationElement itemToSelect = comboboxItem[item];
+
+        //Finding the pattern which need to select
+        SelectionItemPattern selectPattern = (SelectionItemPattern)itemToSelect.GetCurrentPattern(SelectionItemPattern.Pattern);
+        selectPattern.Select();
+       }
     }
+ 
 }
+
+
